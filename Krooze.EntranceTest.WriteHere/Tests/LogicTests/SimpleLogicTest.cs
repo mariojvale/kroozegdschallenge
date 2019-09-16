e63e2182473a56993ced529a4af10228c9da8df5
mariojvale <55214256+mariojvale@users.noreply.github.com>
2019-09-16 13:59:25 -0300

    Add files via upload

--- Krooze.EntranceTest.WriteHere/Tests/LogicTests/SimpleLogicTest.cs
+++ Krooze.EntranceTest.WriteHere/Tests/LogicTests/SimpleLogicTest.cs
@@ -1,4 +1,5 @@
 ﻿using Krooze.EntranceTest.WriteHere.Structure.Model;
+using System;
 
 namespace Krooze.EntranceTest.WriteHere.Tests.LogicTests
 {
@@ -7,14 +8,48 @@ namespace Krooze.EntranceTest.WriteHere.Tests.LogicTests
         public decimal? GetOtherTaxes(CruiseDTO cruise)
         {
             //TODO: Based on the CruisesDTO object, gets if there is some other tax that not the port charge
-            return null;
+            decimal taxs = new decimal();
+
+            taxs = cruise.TotalValue - cruise.CabinValue - cruise.PortCharge;
+            
+            return taxs;
         }
 
         public bool? IsThereDiscount(CruiseDTO cruise)
         {
             //TODO: Based on the CruisesDTO object, check if the second passenger has some kind of discount, based on the first passenger price
             //Assume there are always 2 passengers on the list
-            return null;
+
+            bool disc = new bool();
+            decimal value1 = new decimal();
+            decimal value2 = new decimal();
+            decimal result = new decimal();
+         
+
+            foreach (var item in cruise.PassengerCruise)
+            {
+                if (item.PassengerCode.Equals("1"))
+                {
+                    value1 = item.Cruise.CabinValue;
+
+                }
+                else if (item.PassengerCode.Equals("2"))
+                {
+                    value2 = item.Cruise.CabinValue;
+                }
+
+            }
+
+            if (value1 > value2)
+            {
+                return true;
+
+
+            }
+            else {
+                return false;
+            }
+                  
         }
 
         public int? GetInstallments(decimal fullPrice)
@@ -22,8 +57,27 @@ namespace Krooze.EntranceTest.WriteHere.Tests.LogicTests
             //TODO: Based on the full price, find the max number of installments
             // -The absolute max number is 12
             // -The minimum value of the installment is 200
+            
+            int parc_max = 12;
+            int min_value = 200;
+            int result = 0;
+            int preco = Convert.ToInt32(fullPrice);
 
-            return null;
+            result = preco / min_value;
+
+            if (result < parc_max && result > 0)
+            {
+                return result;
+            }
+            else if (result.Equals(0))
+
+                return 1;
+
+            else
+            {
+                return parc_max;
+            }
+
         }
     }
 }
--- Krooze.EntranceTest.WriteHere/Tests/LogicTests/XMLTest.cs
+++ Krooze.EntranceTest.WriteHere/Tests/LogicTests/XMLTest.cs
@@ -1,4 +1,14 @@
 ﻿using Krooze.EntranceTest.WriteHere.Structure.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Krooze.EntranceTest.WriteHere.Tests.LogicTests
 {
@@ -8,8 +18,54 @@ namespace Krooze.EntranceTest.WriteHere.Tests.LogicTests
         {
             //TODO: Take the Cruises.xml file, on the Resources folder, and translate it to the CruisesDTO object,
             //you can do it in any way, including intermediary objects
-              
-            return null;
+                        
+            XmlDocument oXML = new XmlDocument();
+            CruiseDTO cruise = new CruiseDTO();
+            PassengerCruiseDTO passengerCruise = new PassengerCruiseDTO();
+            List<PassengerCruiseDTO> PassengerCruise = new List<PassengerCruiseDTO>();
+            
+
+            //Define o caminho do arquivo XML 
+            string ArquivoXML = "C:\\Users\\alvaro.gouveia\\Desktop\\kroozegdschallenge-master\\Krooze.EntranceTest.WriteHere\\Resources\\Cruises.xml";
+            //carrega o arquivo XML
+            oXML.Load(ArquivoXML);
+
+            XDocument xmlDoc = XDocument.Load(ArquivoXML);
+             
+            foreach (var cliente in xmlDoc.Descendants("Cruises"))
+            {
+                cruise.CruiseCode = cliente.Element("CruiseId").Value;
+                cruise.ShipName = cliente.Element("ShipName").Value;
+                cruise.TotalValue = decimal.Parse(cliente.Element("TotalCabinPrice").Value.ToString().TrimEnd('0'));
+                cruise.PortCharge = decimal.Parse(cliente.Element("PortChargesAmt").Value.ToString().TrimEnd('0'));
+                cruise.CabinValue = decimal.Parse((cliente.Element("CabinPrice").Value).ToString().TrimEnd('0'));               
+                
+            }
+
+            
+            foreach (var cliente in xmlDoc.Descendants("Pax"))
+            {
+                passengerCruise = new PassengerCruiseDTO();
+                passengerCruise.Cruise = new CruiseDTO();
+                
+                passengerCruise.PassengerCode = cliente.FirstAttribute.Value; //oXML.SelectSingleNode("Cruises/CategoryPriceDetails/Pax").Attributes.GetNamedItem("PaxID").Value;
+                passengerCruise.Cruise.CabinValue = decimal.Parse(oXML.SelectSingleNode("Cruises").ChildNodes[21].ChildNodes[0].ChildNodes[10].ChildNodes[5].InnerText.Trim('0'));
+                passengerCruise.Cruise.PortCharge = decimal.Parse(oXML.SelectSingleNode("Cruises").ChildNodes[21].ChildNodes[0].ChildNodes[11].ChildNodes[5].InnerText.Trim('0'));
+                passengerCruise.Cruise.TotalValue = decimal.Parse(oXML.SelectSingleNode("Cruises").ChildNodes[21].ChildNodes[0].ChildNodes[8].InnerText.Trim('0'));
+                passengerCruise.Cruise.CruiseCode = cruise.CruiseCode;
+                passengerCruise.Cruise.ShipName = cruise.ShipName;
+
+                PassengerCruise.Add(passengerCruise);
+            }
+
+            cruise.PassengerCruise = PassengerCruise;
+          
+            return cruise ;
+        
+
+            
+
+            
         }
     }
 }
